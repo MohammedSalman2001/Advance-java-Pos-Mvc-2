c6fee0a9c834ac335a37f8b354f0e828398428bf
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-08-22 04:35:02 +0530

    login & signup mvc database finalize

--- src/com/davstack/pos/controller/LoginFormController.java
+++ src/com/davstack/pos/controller/LoginFormController.java
@@ -1,14 +1,21 @@
 package com.davstack.pos.controller;
 
+import com.davstack.pos.db.DBConnection;
+import com.davstack.pos.util.PasswordManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginFormController {
     public AnchorPane context;
@@ -16,8 +23,29 @@ public class LoginFormController {
     public TextField txtPassword;
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
+        System.out.println("salman");
 
         //login
+
+        try {
+            Connection connection = DBConnection.getDbConnection().getConnection();
+            String sql="SELECT * FROM user";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                if(PasswordManager.checkPssword(txtPassword.getText(),resultSet.getString(2))){
+                    System.out.println("Completed login");
+                }else {
+                    new Alert(Alert.AlertType.WARNING,"Password Wrong !").show();
+                }
+
+            }else {
+                new Alert(Alert.AlertType.WARNING,"Customer Not Found!").show();
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
--- src/com/davstack/pos/controller/SignUpFormController.java
+++ src/com/davstack/pos/controller/SignUpFormController.java
@@ -1,13 +1,19 @@
 package com.davstack.pos.controller;
 
+import com.davstack.pos.db.DBConnection;
+import com.davstack.pos.util.PasswordManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SignUpFormController {
     public AnchorPane context;
@@ -15,6 +21,31 @@ public class SignUpFormController {
     public TextField txtPassword;
 
     public void btnRegisterNowOnAction(ActionEvent actionEvent) {
+
+        try {
+            Connection connection = DBConnection.getDbConnection().getConnection();
+            String sql="INSERT  INTO user VALUES (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,txtEmail.getText());
+            preparedStatement.setString(2, PasswordManager.enCryptPassword(txtPassword.getText()));
+            boolean isSaved = preparedStatement.executeUpdate() > 0;
+            if(isSaved){
+                new Alert(Alert.AlertType.INFORMATION,"User Saved !").show();
+                clerarFlied();
+            }else {
+                new Alert(Alert.AlertType.WARNING,"Try Again").show();
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+           e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+        }
+
+    }
+
+    private void clerarFlied() {
+        txtPassword.clear();
+        txtEmail.clear();
     }
 
     public void btnAlreadyHaveAnAction(ActionEvent actionEvent) throws IOException {
