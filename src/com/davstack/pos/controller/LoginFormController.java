e840242bfee2fca94dd8c3ea2d2692127efc50c0
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-08-22 03:43:44 +0530

    finalize login & signup ul

--- src/com/davstack/pos/controller/LoginFormController.java
+++ src/com/davstack/pos/controller/LoginFormController.java
@@ -1,4 +1,36 @@
 package com.davstack.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public TextField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+        //login
+
+    }
+
+    public void btnCreateNewAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignUpForm");
+
+
+    }
+
+    public void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
+    }
 }
--- src/com/davstack/pos/controller/SignUpFormController.java
+++ src/com/davstack/pos/controller/SignUpFormController.java
@@ -1,4 +1,30 @@
 package com.davstack.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SignUpFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public TextField txtPassword;
+
+    public void btnRegisterNowOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAlreadyHaveAnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
+    }
+
+    public void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
+    }
 }
